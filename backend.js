7859e5d67bf998661357174d9781ca9605f7e6f6
JhonWeyffer <jhonatasweyffer@gmail.com>
2025-09-14 23:28:55 -0400

    Correção de bugs

--- backend.js
+++ backend.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors({
   origin: (origin, callback) => {
-    const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:5500', 'http://localhost:3000'];
+    const allowedOrigins = ['https://click-beatiful.netlify.app'];
     if (!origin || allowedOrigins.includes(origin)) return callback(null, true);
     return callback(new Error('Not allowed by CORS'));
   },
